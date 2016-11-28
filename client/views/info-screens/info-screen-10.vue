6c12bcd1e9b77c0a22a19c43c3b8251663cfcb49
Arthur Poliakov <poliakov.arthur@gmail.com>
2016-11-28 17:05:31 +0200

    add last popup on landing page

---
+++ client/views/info-screens/info-screen-10.vue
@@ -0,0 +1,19 @@
+<style src="./info-screen.pcss"></style>
+<template lang="pug">
+.info-screen
+  .description._10 DIRECTBOT - это eCommerce платформа для InstАgram магазинов, к которой #[br] вы можете подключить #[br] себя и команду
+</template>
+
+<script>
+export default {
+  methods: {
+    closePage(){
+
+      this.$emit('info-popup.close')
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
