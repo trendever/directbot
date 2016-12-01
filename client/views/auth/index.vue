7f55325ed74d193cbd38729f0df9ffaa10edc623
Arthur Poliakov <poliakov.arthur@gmail.com>
2016-12-01 16:00:23 +0200

    fix reg screen desk

--- client/views/auth/index.vue
+++ client/views/auth/index.vue
@@ -8,7 +8,7 @@
         h1.accept Вход и регистрация
       .column-desktop-50.column-desktop-right(v-if="showTitleSlider")
         img(src="./img/directbot.png").logo
-        p.paragraph Перед подключением оператора, #[br] создайте или войдите #[br] в свою учетную запись
+        p.paragraph Перед подключением оператора, #[br] создайте или войдите #[br(v-if="isMobile")] в свою #[br(v-if="!isMobile")] учетную запись
       .column-desktop-50
         .bottom-container(:class='{"opened-key-board":!showTitleSlider}')
           .input-container
