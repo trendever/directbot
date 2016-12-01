400294b2ee3b10faf0a62f71d3f22ae8fc5d4537
Arthur Poliakov <poliakov.arthur@gmail.com>
2016-12-01 16:00:01 +0200

    add button on popup

--- client/views/info-screens/info-screen-10.vue
+++ client/views/info-screens/info-screen-10.vue
@@ -2,16 +2,25 @@
 <template lang="pug">
 .info-screen
   .description._10 DIRECTBOT - это eCommerce платформа для InstАgram магазинов, к которой #[br(v-if="isMobile")] вы можете подключить #[br(v-if="isMobile")] себя и команду
+  button(v-on:click="closeView").btn.btn_primary.__orange.__xl.fast__big__btn.continue-btn ПРОДОЛЖИТЬ
 </template>
 
 <script>
 export default {
-  methods: {
-    closePage(){
-
-      this.$emit('info-popup.close')
-    }
-  }
+  data(){
+		return {
+			currentView: this.$route.params.id
+		}
+	},
+	methods:{
+		closeView(){
+			if (this.isMobile){
+				history.back();
+			}else{
+				parent.postMessage({name:"Close"}, "*");
+			}
+		}
+	}
 }
 </script>
 
