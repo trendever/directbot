cc2080fad8157e2a7901cf1bab7a1ceb9a58e025
lopan <anlopan@gmail.com>
2016-12-05 12:32:18 +0500

    Photos scroll test

--- client/components/photos/index.vue
+++ client/components/photos/index.vue
@@ -1,8 +1,7 @@
 <template lang="pug">
 .photos
-  ul.photos-flex(:style="{ paddingTop: space + 'px'}")
-    template(v-for="photo, index in listProducts")
-      single(:product="photo.data", :key="photo.id", :class-name="'p-item-' + index")
+  template(v-for="photo, index in listProducts")
+    single(:product="photo.data", :key="photo.id", :class-name="'p-item-' + index")
 
   scroll-top
 
@@ -155,107 +154,6 @@ export default {
     },
 
     infiniteScroll(){
-
-/*    let photos = this.$store.state.photos.lists[this.list];
-
-      if(photos.padding) {
-
-        this.padding = photos.padding;
-
-      }
-
-      if(localStorage.getItem('scrollCnt')){
-
-        let { idStart, idEnd } = localStorage.getItem('scrollCnt');
-
-
-
-      }
-
-      if( photos.list.length) {
-
-      }
-
-      if( !photos.list.length ) {
-
-        products.find({limit:50, offset: this.offset, shop_id: this.shopId }).then(data=>{
-
-          photos.list = data;
-
-          photos.idStart = 0;
-
-          photos.idEnd = data.length;
-
-        })
-
-      }
-
-      this.windowListener = listen(window, 'scroll',()=>{
-
-        localStorage.setItem(`${this.$route.name}.scroll`, window.scrollY);
-
-        let direction = window.scrollY - this.oldScroll < 0 ? false : true;
-
-        this.oldScroll = window.scrollY;
-
-        let { scrollTop, scrollHeight } = document.body;
-
-        if(this.off) return;
-
-        if(direction) {
-
-          let xItem = document.querySelector(`.p-item-${Math.ceil(this.getPhotos.length/3)}`);
-          xItem.style.background = 'red';
-
-          if( xItem.getBoundingClientRect().bottom < 0 ) {
-
-            this.off = true;
-            this.offset += 20;
-
-            products.find({limit: 20, offset: this.offset, shop_id: this.shopId  }).then(data=>{
-
-              data.forEach(item=>{
-
-                this.$store.state.photos.list.push(item)
-
-              });
-
-              photos.idStart += 10;
-
-              photos.idEnd += 10;
-
-              this.padding += xItem.offsetHeight * 5
-
-              this.off = false;
-
-            })
-
-
-
-          }
-        } else {
-
-            let xItem = document.querySelector(`.p-item-${Math.ceil(this.getPhotos.length/3)}`);
-
-            xItem.style.background = 'red';
-
-            if( xItem.getBoundingClientRect().top > window.innerHeight ) {
-
-              if(photos.idEnd < 40) {
-
-                return;
-
-              }
-
-              photos.idStart -= 10;
-
-              photos.idEnd -= 10;
-
-              this.padding -= Math.abs(xItem.offsetHeight * 5)
-
-            }
-        }
-      })*/
     }
   },
 
