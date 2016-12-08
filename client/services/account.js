3049b9e5de3fa3ce8eb1a7dbf7927a53eccc2143
riffca <riffca@yandex.ru>
2016-12-08 14:22:36 +0300

    add account service methods

--- client/services/account.js
+++ client/services/account.js
@@ -14,16 +14,62 @@ export function add(username, password) {
   // CONFIG = config ? config : CONFIG;
 
   return new Promise( (resolve, reject) => {
-  	let request = {username,password}
+
+  	let request = { username, password }
+
   	request.role = "User";
+
   	request.prefer_email = true;
 
     channel.req('add', 'account', request).then( data => {
-		console.log("ADD ACCOUNT");
-		console.log(data);
+
+  		console.log("ADD ACCOUNT");
+
+  		resolve(data);
+
     }).catch( error => {
+
       	console.error('ADD ACCOUNT', error);
+
     });
 
   });
+}
+
+export function list({ role, with_invalids, with_non_owned }) {
+
+  return new Promise( (resolve, reject) => {
+
+    role = "User";
+
+    channel.req('list', 'account', { role, with_invalids, with_non_owned }).then( data => {
+
+      resolve(data);
+
+    }).catch( error => {
+
+        console.error('LIST ACCOUNT', error);
+
+    });
+
+  });
+}
+
+
+export function confirm(code) {
+
+  return new Promise( (resolve, reject) => {
+
+    channel.req('confirm', 'account', { code } ).then( data => {
+
+      resolve(data);
+
+    }).catch( error => {
+
+        console.error('CONFIRM ACCOUNT', error);
+
+    });
+
+  });
+
 }
