280003404cee829aa10138f27fb9ab46ee83b482
riffca <riffca@yandex.ru>
2016-12-08 15:32:15 +0300

    add need confirm code flag

--- client/views/connect-bot/index.vue
+++ client/views/connect-bot/index.vue
@@ -9,13 +9,13 @@
         h1.accept Подключение оператора
       .column-desktop-50.column-desktop-right
         img(src="./directbot.png").logo
-        p.paragraph.enter-screen
+        p.paragraph.enter-screen(v-if="!needConfirmCode")
           span.bold Безопасность вашего Instagram #[br(v-if="!isMobile")] профиля очень важна для нас! #[br]
           span.light Поэтому мы не храним пароль #[br] профиля и используем его только #[br] при подключения бота
-        p.paragraph.enter-code-screen(v-if="false")
+        p.paragraph.enter-code-screen(v-if="needConfirmCode")
           span.bold Пожалуйста, #[br] введите 6-ти значный код, #[br]
           span.light высланный на email или телефон, #[br] указанные при регистрации #[br] профиля
-          span.bold  {username}
+          span.bold  {{ login }}
           span.light  в Instаgram
         p.paragraph.code-accept(v-if="false")
           span.bold Код подтвержден #[br]
@@ -23,7 +23,7 @@
       .column-desktop-50
         .bottom-container(:class='{"opened-key-board":!showTitleSlider}')
           .input-container
-            .input.name
+            .input.name(v-if="!needConfirmCode")
               i.ic-insta-name
               input(type='text',
                 autocomplete="off",
@@ -38,7 +38,7 @@
                 v-if='login',
                 @click='login = ""')
                 i.ic-close.clear
-            .input.phone(id="pass-inp")
+            .input.phone(v-if="!needConfirmCode", id="pass-inp")
               i.ic-pass
               input(type='tel',
                 autocomplete="off",
@@ -52,13 +52,18 @@
               .input__clear-btn(
                 v-if='phone')
                 i.ic-close.clear
-            .input(id="code", v-if="false")
+            .input(id="code", v-if="needConfirmCode")
               i.ic-code
               input(
+                v-model="code"
                 placeholder='Введите код')
           .btn-container
             button.btn.btn_primary.__orange.__xl.fast__big__btn.btn_fixed-bottom(
+              v-if="!needConfirmCode",
               @click='connectBot') ПОДКЛЮЧИТЬ ОПЕРАТОРА
+            button.btn.btn_primary.__orange.__xl.fast__big__btn.btn_fixed-bottom(
+              v-if="needConfirmCode",
+              @click='connectBot') ПОДТВЕРДИТЬ
             .link-container.new-sms
               a.link-bottom(href='#')
                 | Мне нужна помощь
@@ -71,24 +76,47 @@ export default {
 
   data(){
     return {
+      code: '',
       password: '',
-      login: ''
+      login: '',
+      needConfirmCode: false
     }
   },
   methods:{
 
     connectBot(){
 
-      accountService.add( this.login, this.password ).then(data=>{
+      accountService
+        .add( this.login, this.password )
 
-        if(data.need_code === false && data.success){
+        .then(data=>{
 
-          this.$router.push({name: 'profile'});
+          if(!data.need_code && data.success){
 
-        }
+            this.$router.push( { name: 'profile' } );
+            return;
+
+          }
+
+          if(data.need_code){
+
+            this.needConfirmCode = true;
+
+          }
 
       })
 
+
+    },
+
+    confirm() {
+
+      accountService
+        .confirm(this.code).then(data=>{
+
+
+
+        })
 
     }
 
