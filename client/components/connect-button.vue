0d82b282a8e221010bd896d280ab1a0395aa9ee9
riffca <riffca@yandex.ru>
2016-12-16 13:59:42 +0300

    rename button

--- client/components/connect-button.vue
+++ client/components/connect-button.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 
 button.btn.btn_primary.__orange.__xl.fast__big__btn.btn_fixed-bottom.turn-on-bot-btn(
-  @click="$router.push({name: 'connect-bot'})") ПОДКЛЮЧИТЬ ОПЕРАТОРА
+  @click="$router.push({name: 'connect-bot'})") ПОДКЛЮЧИТЬ INSTAGRAM
 
 </template>
 
