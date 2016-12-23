546429a4c8b4cf39f9bd36f92b5f4d566a1404b7
riffca <riffca@yandex.ru>
2016-12-23 18:50:36 +0300

    logOut arrowBakc if infoChat

--- client/views/chat/chat-header.vue
+++ client/views/chat/chat-header.vue
@@ -44,6 +44,13 @@
          //this.userImage = require('base/img/logo.png');
       },
       leftBtnAction(){
+
+        if(window.infoQuestions){
+          this.$store.dispatch('logOut')
+          this.$router.push({name: 'home'})
+          return;
+        }
+
         if (this.isFake){
           if (this.$route.name === window.before.name && window.before.prev){
             this.$router.push({ name: window.before.prev.name, params: window.before.prev.params})
