cdb9797a1b7fd0027acd8d1877856eb1bab01a5d
riffca <riffca@yandex.ru>
2016-12-29 00:16:53 +0300

    hide tabbar for fake

--- client/components/navbar/navbar.vue
+++ client/components/navbar/navbar.vue
@@ -1,7 +1,7 @@
 <style src='./navbar.pcss'></style>
 <template lang="pug">
 
-.navbar-cnt(v-if='isAuth && isMobile')
+.navbar-cnt(v-if='isAuth && isMobile && !isFake')
   .navbar.section__content
     .navbar_i(:class='{"__active": current=="feed"}', @click='$router.push({name: "home"}), scrollTop')
       .navbar_i_wrap
