bd9265b33460fb5ab2c7e435398abfe46111e194
riffca <riffca@yandex.ru>
2017-02-03 12:56:56 +0300

    fix text size

--- client/views/home/index.vue
+++ client/views/home/index.vue
@@ -15,7 +15,7 @@
       .hero__content__center-logo(v-if="!isMobile")
         img(src="./img/directbot-main-logo.png")
       .hero__content__description
-        | Оператор #[br(v-if="isMobile")] магазина в Instagram #[br]
+        | Оператор магазина #[br(v-if="isMobile")] в Instagram #[br]
         span.time__text Всегда на связи с клиентами
       .hero__content__set-up
         button(v-on:click="$router.push({name: 'auth'})").set-up-btn ПОПРОБОВАТЬ БЕСПЛАТНО
