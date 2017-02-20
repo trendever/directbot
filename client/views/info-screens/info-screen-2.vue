2e56377d62c9673d1090f6d7cdacb1d7e5874845
riffca <riffca@yandex.ru>
2017-02-20 22:27:26 +0300

    change text

--- client/views/info-screens/info-screen-2.vue
+++ client/views/info-screens/info-screen-2.vue
@@ -1,8 +1,26 @@
 <style src="./info-screen.pcss"></style>
 <template lang="pug">
 .info-screen
-  .description._2 Узнает для вас телефон покупателя, отправит ему ссылку на ваш сайт #[br(v-if="isMobile")] и товар. #[br(v-if="!isMobile")] Потом узнает размеры, сверит #[br] с наличием. Спросит адрес доставки или запишет #[br(v-if="isMobile")] в шоу-рум. Клиент доволен быстрой реакцией, а у вас останется больше времени #[br(v-if="isMobile")] на личное общение
+  .description._2
+
+    | проконсультирует покупателя,#[br]
+    | узнает его желания и сверит#[br]
+    | информацию о товаре#[br(v-if="isMobile")] с вашим#[br(v-if="!isMobile")]
+    | сайтом или#[br(v-if="isMobile")] таблицей остатков#[br(v-if="!isMobile")]
+    | товара.#[br(v-if="isMobile")] Спросит адрес доставки#[br]
+    | или запишет в шоу-рум.
+
 </template>
 
-<style lang="css">
+<style lang="postcss">
+@import 'style/vars/vars.pcss';
+
+.info-screen .description._2 {
+
+  padding-top: 25%;
+  @media (--mobile) {
+    padding-top: 60%;
+  }
+
+}
 </style>
