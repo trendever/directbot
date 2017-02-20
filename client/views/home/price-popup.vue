d8563eb486eb5859f62ee004f0a7ef695f76a224
riffca <riffca@yandex.ru>
2017-02-20 19:00:27 +0300

    change pop up

--- client/views/home/price-popup.vue
+++ client/views/home/price-popup.vue
@@ -3,23 +3,26 @@
 
   .price-title Сколько стоит Directbot?
 
-  .price-content
 
-    .price-text
-      | Доступ в панель Directbot стоит#[br]
-      | 1990 руб. В месяц. Оттуда вы и ваша#[br]
-      | команда можете сами отвечать клиентам#[br]
-      | в Instagram. Пробуйте бесплатно!#[br]
+  .wrap-block
 
-    .price-image
+    .price-content
+
+      .price-text
+        | Доступ в панель Directbot стоит#[br]
+        | 1990 руб. В месяц. Оттуда вы и ваша#[br]
+        | команда можете сами отвечать клиентам#[br]
+        | в Instagram. Пробуйте бесплатно!#[br]
+
+      .price-image
 
 
-    .price-caption
+      .price-caption
 
-      | Либо попробуйте услуги#[br]
-      | нашего оператора, который сам#[br]
-      | обслуживает ваших клиентов.#[br]
-      | Тарифы доступны после регистрации#[br]
+        | Либо попробуйте услуги#[br]
+        | нашего оператора, который сам#[br]
+        | обслуживает ваших клиентов.#[br]
+        | Тарифы доступны после регистрации#[br]
 
   .price-button ПОПРОБОВАТЬ БЕСПЛАТНО
 
@@ -48,7 +51,13 @@ export default {
 
 
   @media ( --mobile ) {
+
     font-size: $font__medium;
+  }
+  .wrap-block {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
   }
 
   .price-title {
@@ -62,24 +71,39 @@ export default {
     text-align: left;
     @media (--mobile) {
       text-align: center;
+      width: 100%;
+    }
+
+    width: 50%;
+
+    .price-text, .price-caption {
+      padding: 24px 0;
     }
 
   }
 
   .price-button {
+    cursor: pointer;
+    margin: 0 auto;
     text-align: center;
     color: black;
-
-    font-size: $font__medium;
+    font-size: $font__normal;
     background: $color-orange;
+    position: relative;
+    height: 75px;
+    width: 420px;
+    margin-top: 24px;
+    line-height: 75px;
+    border-radius: 5px;
+
+
     @media (--mobile) {
+      font-size: $font__medium;
       position: absolute * 0 0 0;
       height: 100px;
       width: 100%;
       line-height: 100px;
-     }
-
+    }
   }
-
 }
 </style>
