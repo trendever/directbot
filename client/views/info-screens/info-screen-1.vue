ab80f00aad324519a8e182782118c036124e20fd
riffca <riffca@yandex.ru>
2017-02-20 22:46:23 +0300

    fix

--- client/views/info-screens/info-screen-1.vue
+++ client/views/info-screens/info-screen-1.vue
@@ -1,6 +1,13 @@
 <style src="./info-screen.pcss"></style>
 <template lang="pug">
 .info-screen
-  .description._1 Наш опытный оператор, вооружившись Directbot-ом, обслужит всех ваших клиентов быстро и в #[br(v-if="isMobile")] любое время дня #[br(v-if="!isMobile")] и ночи. #[br(v-if="isMobile")] Он вежливо поговорит #[br] с покупателем прямо #[br(v-if="isMobile")] в Instagram Direct, #[br(v-if="isMobile")] как консультант от #[br(v-if="isMobile")] имени вашего магазина. #[br(v-if="isMobile")] Вы не тратите время #[br(v-if="isMobile")] на клиентов, которые #[br(v-if="!isMobile")] не покупают
+  .description._1
+    | Наш опытный оператор, вооружившись Directbot-ом, обслужит всех ваших клиентов быстро и в #[br(v-if="isMobile")]
+    | любое время дня #[br(v-if="!isMobile")] и ночи. #[br(v-if="isMobile")]
+    | Он вежливо поговорит #[br] с покупателем прямо #[br(v-if="isMobile")]
+    | в Instagram Direct, #[br(v-if="isMobile")] как консультант от
+    | #[br(v-if="isMobile")] имени вашего магазина. #[br(v-if="isMobile")]
+    | Вы не тратите время #[br(v-if="isMobile")]
+    | на клиентов, которые не покупают
 </template>
 
