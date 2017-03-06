c7671a2df01f1598de68bf6ff69e4bfa324ba779
riffca <riffca@yandex.ru>
2017-03-06 15:41:18 +0300

    add scroll logic

--- client/views/chat-list/temp.js
+++ client/views/chat-list/temp.js
@@ -11,27 +11,32 @@
 
         .header__nav__i.header__text(
           :class='{_active: getLeadTab === "customer"}',
-          @click='$store.dispatch("setTab","customer")')
+          @click='$store.dispatch("setTab","customer")',
+          v-if="getLeadTab === 'customer'")
           span Чаты с продавцами
 
         .header__nav__i.header__text(
           :class='{_active: getLeadTab === "seller"}',
-          @click='$store.dispatch("setTab", "seller")')
+          @click='$store.dispatch("setTab", "seller")',
+          v-if="getLeadTab === 'seller'")
           span Чаты с покупателями
 
     .section.top.bottom
       .section__content
         .chat-list(v-bind:style="styleObject", ref="chatList")
-          .space-block(:style="{height:spaceBlock+'px'}")
+          .space-block(:style="{ height: spaceBlock + 'px', position: 'relative' }")
           chat-list-item(
-            v-for='lead, index in leadsArray',
+            v-for='lead, index in sortedList',
             :lead='lead',
             :key="lead.id",
             ref="item",
-            :class="{'scrl-down': index === downCount - 1,'scrl-top':index === 0 }")
-          .visible-block(ref="visibleBlock")
+            :class="{'scrl-down': index === 6, 'scrl-top': index === 0 }")
 
-      //-.chat-list-cnt-is-empty__banner.directbot-banner(
+
+
+
+
+      .chat-list-cnt-is-empty__banner.directbot-banner(
         v-if="!botActivity && getBannerInfo.indexOf('chat-banner') === -1", :class="{'turn-bottom': sortedList.length}")
 
         i.ic-close(@click="$store.dispatch('closeStat', 'chat-banner')")
@@ -41,22 +46,22 @@
         span.want
           | Instagram Direct
 
-      //-.chat-list-cnt-is-empty(v-if="!sortedList.length")
+      .chat-list-cnt-is-empty(v-if="!sortedList.length")
         .chat-list-cnt-is-empty__container Нет чатов,#[br]
         span
           | потому что мы не #[br]
           | видим ваших заказов! #[br]
           a.link-err(@click="openSupport") Это ошибка
 
-      //-template(v-if="!botActivity")
+      template(v-if="!botActivity")
         connect-button(v-if="!sortedList.length")
 
-  //-.directbot-navbar(v-if="isMobile && isAuth")
+  .directbot-navbar(v-if="isMobile && isAuth")
     navbar-component(current='chat')
-  //-.directbot-right-nav
+  .directbot-right-nav
     right-nav-component(current="chat")
-  .block-click(@click="resetBlock")
-    scroll-top
+
+  //-scroll-top
   //-app-loader.list-loader(v-if="!needLoadLeads")
 
 
@@ -101,20 +106,19 @@
           pointerEvents: 'auto'
         },
         currentPan: 0,
-        //
+
+
+        //scroll two way
         lastScroll: 0,
         currentScroll: 0,
-        downCount: 15
+        spaceBlock: 0
 
       }
     },
-    created(){
-      this.spaceBlock = this.$store.state.leads.scrollLogic[this.getLeadTab].spaceBlock;
-    },
-    beforeDestroy(){
-      this.leadsArray = []
-    },
+
     mounted(){
+
+      this.spaceBlock = this.$store.state.leads.scrollLogic.seller.spaceBlock;
 
       if (this.isFake){
 
@@ -152,6 +156,7 @@
 
             }, 200 );
 
+
             store.dispatch('setScrollLeads', {
 
               scrollTop: document.body.scrollTop,
@@ -159,9 +164,13 @@
 
             });
 
+
             if ( this.needLoadLeads ) {
 
-              if (this.$refs.visibleBlock.getBoundingClientRect().bottom <= window.innerHeight ) {
+              const full_scroll = document.body.scrollHeight;
+              const diff_scroll = full_scroll - document.body.scrollTop;
+
+              if ( diff_scroll < 2500 ) {
 
                 this.needLoadLeads = false;
 
@@ -175,13 +184,44 @@
 
             }
 
-            this.optimizeChange();
+            //--------SCROLL LEADS------------------
+            let stateLeads = this.$store.state.leads;
+            let scrollPoint = document.body.scrollTop;
+            let  { idStart }= this.scrollIds;
 
+            this.currentScroll = scrollPoint;
+
+
+            if(this.lastScroll < this.currentScroll){
+              let target = document.querySelector('.scrl-down');
+              if(target.getBoundingClientRect().bottom <= 0){
+                this.spaceBlock += 7 * target.offsetHeight;
+                stateLeads.scrollLogic.seller.idStart += 7;
+                stateLeads.scrollLogic.seller.idEnd += 7;
+                stateLeads.scrollLogic.seller.spaceBlock = this.spaceBlock;
+              }
+              colorLog('scrollDown')
+              this.lastScroll = scrollPoint;
+            }
+            if(this.lastScroll > this.currentScroll){
+              let target = document.querySelector('.scrl-top');
+              if(target.getBoundingClientRect().top >= 0 && idStart > 0){
+                this.spaceBlock -= 7 * target.offsetHeight;
+                stateLeads.scrollLogic.seller.idStart -= 7;
+                stateLeads.scrollLogic.seller.idEnd -= 7;
+                stateLeads.scrollLogic.seller.spaceBlock = this.spaceBlock;
+              }
+              colorLog('scrollUp')
+              this.lastScroll = scrollPoint;
+            }
+            //------------------------------------
           }
 
         })() );
 
         leads.onEvent( this.onEvent );
+
+        this.run();
 
       } else {
         this.$router.push( { name: 'signup' } );
@@ -217,106 +257,56 @@
         'getScroll',
         'getHasMore'
       ]),
+      scrollIds(){
+        return this.$store.state.leads.scrollLogic.seller;
+      },
+      sortedList(){
+        return this.leadsArray.slice( 0, this.getLeadsLengthList ).sort((a,b)=>{
+          return b.updated_at - a.updated_at;
+        })
+      },
 
       leadsArray(){
+        //проверка на удаленные лиды
+        if(this.$store.state.leads.tab === 'customer') {
 
-        function removeCancelLeads(item){
-          return !(item.cancel_reason === 2) && !(item.cancel_reason === 1);
+          // let leads = this.getLeads.filter(item=> {
+
+          //   return !(item.cancel_reason === 2) && !(item.cancel_reason === 1);
+
+          // });
+
+          //только чаты с продавцами
+          this.$store.dispatch('setTab', 'seller');
+
+          return [];
+
         }
 
-        let leads = [];
+        if(this.$store.state.leads.tab === 'seller') {
 
-        if(this.getLeadTab === 'customer') {
-          leads = this.getLeads.filter(removeCancelLeads);
+          let leads = this.getLeads.filter( item => {
+
+            return !(item.cancel_reason === 2) && !(item.cancel_reason === 1);
+
+          });
+
+          let { idStart, idEnd } = this.scrollIds;
+
+          return leads.slice(idStart, idEnd);
+
         }
+      }
+    },
+    events: {
 
-        if(this.getLeadTab === 'seller') {
-          leads = this.getLeads.filter(removeCancelLeads);
-        }
+      'closeDeleteLead'(){
 
-        let { idStart, idEnd } = this.$store.state.leads.scrollLogic[this.getLeadTab];
-
-        return leads.slice(idStart, idEnd).sort((a,b)=>b.updated_at - a.updated_at);
-
-
-      },
-      spaceBlock(){
-
-        return this.$store.state.leads.scrollLogic[this.getLeadTab].spaceBlock;
+        this.$broadcast('closeDelete');
 
       }
-
     },
     methods: {
-
-      resetBlock() {
-        let tab = this.$store.state.leads.scrollLogic[this.getLeadTab];
-        tab.idStart=0;
-        tab.idEnd=30;
-        tab.spaceBlock = 0;
-      },
-
-      optimizeChange(){
-
-        let scrollPoint = document.body.scrollTop;
-        this.currentScroll = scrollPoint;
-
-        let stateLeads = this.$store.state.leads;
-        let idStart = stateLeads.scrollLogic[this.getLeadTab].idStart;
-        let tab = stateLeads.scrollLogic[this.getLeadTab];
-        if(this.lastScroll < this.currentScroll){
-          let target = document.querySelector('.scrl-down');
-          if(target && target.getBoundingClientRect().bottom <= 0 && this.getLeads.length > 20){
-              tab.idStart += this.downCount;
-              tab.idEnd += this.downCount;
-              tab.spaceBlock  += (this.downCount) * target.offsetHeight
-          }
-          colorLog('scrollDown')
-          this.lastScroll = scrollPoint
-        }
-
-        if(this.lastScroll > this.currentScroll){
-          let target = document.querySelector('.scrl-top');
-          if(target && target.getBoundingClientRect().top >= 0 && idStart > 0){
-            tab.idStart -= this.downCount;
-            tab.idEnd -= this.downCount;
-            tab.spaceBlock -= (this.downCount) * target.offsetHeight;
-          }
-          if(target.getBoundingClientRect().bottom >= target.offsetHeight && idStart <= 0){
-            tab.spaceBlock = 0;
-          }
-          colorLog('scrollUp')
-          this.lastScroll = scrollPoint;
-
-        }
-
-        tab.scroll = scrollPoint;
-
-
-        // this.$refs.item.forEach( item => {
-
-        //   let rect = item.$el.getBoundingClientRect();
-
-        //   if(rect.bottom < 0){
-
-        //     stateLeads.scrollLogic[this.getLeadTab].idStart += this.downCount;
-        //     stateLeads.scrollLogic[this.getLeadTab].idEnd += this.downCount;
-        //     stateLeads.scrollLogic[this.getLeadTab].spaceBlock += rect.height;
-
-        //   }
-        //   if(rect.top > window.innerHeight){
-
-        //     stateLeads.scrollLogic[this.getLeadTab].idStart -= this.downCount;
-        //     stateLeads.scrollLogic[this.getLeadTab].idEnd -= this.downCount;
-        //     stateLeads.scrollLogic[this.getLeadTab].spaceBlock = rect.height;
-        //   }
-
-        //   colorLog(item.isVisible)
-
-        // })
-
-      },
-
       openSupport(){
         this.$store
           .dispatch('createLead',config.support_id || 37318)
@@ -333,18 +323,123 @@
                 this.$store.state.leads.seller.unshift(lead)
                 this.$store.state.leads.notify_count[lead.id] = 1
               });
+
+
         }
-      }
+      },
+      run(){
+
+        this.runLoadingLeads().then( () => {
+
+          const { scrollTop } = this.getScroll;
+
+            this.restoreScroll()
+
+        } )
+
+      },
+
+      restoreScroll(){
+
+        return new Promise( ( resolve ) => {
+
+          const add = ( targetHeight ) => {
+
+            const { scrollTop } = this.getScroll;
+
+            /**
+             * Магическое 1000 это кол-во px после scrollTop;
+             * */
+
+            if ( targetHeight < ( scrollTop + 1000 ) ) {
+
+                store.dispatch('loadLeads').then( () => {
+
+                  setTimeout(()=>{
+
+                    this.$nextTick( () => {
+
+                      add( document.body.scrollHeight );
+
+                    } );
+
+                  },1)
+
+                } )
+
+            } else {
+
+              window.scrollTo(0, scrollTop);
+
+              resolve();
+
+            }
+
+          };
+
+          this.$nextTick( () => {
+
+            add( document.body.scrollHeight );
+
+          } );
+
+        } );
+
+      },
+      runLoadingLeads(){
+
+        return new Promise( ( resolve ) => {
+
+        /*  const add = ( scrollHeight ) => {
+
+            if ( document.body.offsetHeight >= scrollHeight ) {
+
+              store.dispatch('loadLeads').then( () => {
+
+                if ( this.getHasMore ) {
+
+                  this.$nextTick( () => {
+
+                    add( document.body.offsetHeight );
+
+                  } );
+
+                } else {
+
+                  resolve()
+
+                }
+
+              } )
+
+            } else {
+
+              resolve();
+
+            }
+
+          };
+
+          this.$nextTick( () => {
+
+            add( document.body.scrollHeight )
+
+          } );*/
+          resolve()
+
+        } );
+
+      },
+
     },
 
     watch: {
       getLeadTab(){
-        this.$nextTick(()=>{
-          document.body.scrollTop = this.$store.state.leads.scrollLogic[this.getLeadTab].scroll;
-        })
+
+        this.run();
+
       }
+
     }
   }
 </script>
-<style lang="postcss">
-</style>
