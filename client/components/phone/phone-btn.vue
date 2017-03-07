336246ccc812c2d39836be8c56e476f5a233c026
riffca <riffca@yandex.ru>
2017-03-07 16:05:37 +0300

    add phone to desktop

--- client/components/phone/phone-btn.vue
+++ client/components/phone/phone-btn.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 
-#phone(slot="center-content" v-if="isMobile && phone")
+#phone(slot="center-content" v-if="phone")
   a(:href="phone")
     img(src="./phone.png")
 
@@ -19,21 +19,34 @@ export default {
 <style lang="postcss">
 @import 'style/vars/vars.pcss';
 #phone {
-  display: none;
+  max-width: 40px;
+  height: auto;
+
+  img {
+    width: 100%;
+    height: auto;
+  }
+  a {
+    display: initial;
+    padding: 15px;
+  }
+
+  @media (--overmobile){
+    position: absolute -5px 20% 0 *;
+    display: inline-block;
+    text-align: center;
+    a {
+      display: inline-block;
+      width: 60px;
+    }
+
+  }
+
   @media (--mobile) {
     box-sizing: content-box;
     display: block;
     position: absolute 23px 45px 0 *;
-    max-width: 40px;
-    height: auto;
-    img {
-      width: 100%;
-      height: auto;
-    }
-    a {
-      display: initial;
-      padding: 15px;
-    }
   }
+
 }
 </style>
