e477b370974cebc53537b3a502d80708c0c75a29
riffca <riffca@yandex.ru>
2017-03-11 12:19:38 +0300

    change popup text

--- client/views/info-screens/info-screen-2.vue
+++ client/views/info-screens/info-screen-2.vue
@@ -3,12 +3,10 @@
 .info-screen
   .description._2
 
-    | проконсультирует покупателя,#[br]
-    | узнает его желания и сверит#[br]
-    | информацию о товаре#[br(v-if="isMobile")] с вашим#[br(v-if="!isMobile")]
-    | сайтом или#[br(v-if="isMobile")] таблицей остатков#[br(v-if="!isMobile")]
-    | товара.#[br(v-if="isMobile")] Спросит адрес доставки#[br]
-    | или запишет в шоу-рум.
+    | ВЫ ИЛИ ЛЮБОЙ ВАШ СОТРУДНИК#[br]
+    | СМОЖЕТЕ НЕ ТОЛЬКО КОНТРОЛИРОВАТЬ#[br.desktop]
+    | ЗАКАЗЫ,#[br.mobile] НО И СОВМЕСТНО ОТВЕЧАТЬ#[br.desktop]
+    | КЛИЕНТАМ НЕ ОТКРЫВАЯ INSTAGRAM#[br.desktop]
 
 </template>
 
@@ -17,7 +15,7 @@
 
 .info-screen .description._2 {
 
-  padding-top: 25%;
+  padding-top: 30%;
   @media (--mobile) {
     padding-top: 60%;
   }
