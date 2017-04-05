26813da17c4e0b2464fc784a31fe0e44c8b723be
riffca <riffca@yandex.ru>
2017-04-06 00:46:58 +0300

    build with size warnings

--- client/index.js
+++ client/index.js
@@ -1,9 +1,11 @@
 import {app} from './app'
 import FastClick from 'fastclick';
 
+import Promise from 'promise-polyfill'
+
+window.Promise = window.Promise || Promise
 
 app.$mount('#app')
-
 
 // window.onmessage = (msg) => {
 //   console.log(msg )
