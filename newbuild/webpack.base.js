00ad103aa86e86e477332214f05d71e8f71a6788
Anton <anlopan@gmail.com>
2017-04-07 01:15:19 +0300

    Change path

--- newbuild/webpack.base.js
+++ newbuild/webpack.base.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     path: _.outputPath,
     filename: '[name].js',
-    publicPath: '/'
+    publicPath: 'dev.directbot.io'
   },
   performance: {
     hints: process.env.NODE_ENV === 'production' ? 'warning' : false
