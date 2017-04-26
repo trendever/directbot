5cf087b8043c60e710b94b2e8a662d8219f7ef43
riffca <riffca@yandex.ru>
2017-04-26 14:53:38 +0300

    fix font

--- client/views/dress-blogger/index.vue
+++ client/views/dress-blogger/index.vue
@@ -88,8 +88,8 @@ export default {
       position: absolute;
       left: 50%;
       top:50%;
-      transform: translate(-50%, -50%);
-      font-size: $font__medium;
+      transform: translate(-50%, -100%);
+      font-size: calc( $font__medium + 10px);
       font-family: $font__family__semibold;
     }
   }
