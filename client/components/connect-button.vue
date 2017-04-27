013b55b0d112567cc710b11219b7a9de16a1a7f4
riffca <riffca@yandex.ru>
2017-04-27 14:56:27 +0300

    change button text trendever

--- client/components/connect-button.vue
+++ client/components/connect-button.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
 
 button.btn.btn_primary.__orange.__xl.fast__big__btn.btn_fixed-bottom.turn-on-bot-btn(
-  @click="$router.push({name: 'connect-bot'})") ПОДКЛЮЧИТЬ INSTAGRAM
+  @click="$router.push({name: 'connect-bot'})")
+  template(v-if="!isTrendever") ПОДКЛЮЧИТЬ INSTAGRAM
+  template(v-if="isTrendever") ПОДКЛЮЧИТЬ БИЗНЕС-ПРОФИЛЬ
 
 </template>
 
