5ae84669a08866b0e87eeb95672709902fdf1861
riffca <riffca@yandex.ru>
2017-05-03 12:55:46 +0300

    change arrow position

--- client/components/header/index.vue
+++ client/components/header/index.vue
@@ -5,7 +5,7 @@
   .header__content
     .wrapper.directbot-color(:class="{'chat-slot': $route.name === 'chat' && isMobile}")
 
-      .logos(v-if="$route.name !== 'chat'")
+      .logos
         i.ic-logo_trendever_txt(v-if="isTrendever", @click="$router.push({name: 'home'})")
         i.ic-logo_directbot_txt(v-if="!isTrendever", @click="$router.push({name: 'home'})")
 
