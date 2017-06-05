385b686e194e7aebebf3168e57c6ee3886d71512
riffca <riffca@yandex.ru>
2017-06-05 16:10:11 +0300

    change text

--- client/components/connect-button.vue
+++ client/components/connect-button.vue
@@ -3,8 +3,7 @@
 button.btn.btn_primary.__orange.__xl.fast__big__btn.btn_fixed-bottom.turn-on-bot-btn(
   @click="$router.push({name: 'connect-bot'})")
   template(v-if="!isTrendever") ПОДКЛЮЧИТЬ INSTAGRAM
-  template(v-if="isTrendever")
-    | ПОДКЛЮЧИТЬ БИЗНЕС
+  template(v-if="isTrendever") НАЧАТЬ БИЗНЕС
 </template>
 
 <style lang="postcss">
