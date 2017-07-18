8fe92296e37d1c00a5cd0d60d1ce90aa174be4ac
riffca <riffca@yandex.ru>
2017-07-18 18:45:18 +0300

    change input chat

--- client/views/chat/index.vue
+++ client/views/chat/index.vue
@@ -1,7 +1,6 @@
 <style src='./styles/chat.pcss'></style>
 <template lang="pug">
-#chat
-  //-(:class="{'directbot-bg': !isTrendever,'trendever-bg': isTrendever}")
+#chat(:class="{'directbot-bg': !isTrendever,'trendever-bg': isTrendever}")
   //-.loader-center(v-if="showLoader"): app-loader
 
   chat-header(:prev-page="prevPage")
